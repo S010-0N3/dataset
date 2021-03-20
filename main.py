225216fb8478a4c89706ec4678c84a1b25fc728a
Kassama Fally <fally59100@gmail.com>
2021-03-20 20:23:05 +0000

    #creer une fonction qui prendra en entre le nom d'une equipe et retournera ces victoires.sf_wins

--- main.py
+++ main.py
@@ -2,8 +2,27 @@
 import csv
 
 f = open("nfl_data.csv")
-csvreader = csv.reader(f)
+nfl = csv.reader(f)
 
-nfl = list(csvreader)
+sf_wins = 0
+for row in nfl:
+  if row[2] == "San Francisco 49ers":
+    sf_wins += 1
 
-print(nfl)
+print(sf_wins)
+
+
+#creer une fonction qui prendra en entre le nom d'une equipe et retournera ces victoires.sf_wins
+f = open("nfl_data.csv")
+nfl = csv.reader(f)
+
+def nfl_wins(team):
+  count = 0
+  for row in nfl:
+    if row[2] == team:
+      count += 1
+  return(count)
+
+
+victoire = nfl_wins(input())
+print(victoire,"victoire")
